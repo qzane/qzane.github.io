b517c95ac1fb92a31933942a23a5ff5509a50945
qzane <qzane@live.com>
2016-08-31 16:38:32 +0800

    Create 160831-speaker.py

---
+++ attachments/160831-speaker.py
@@ -0,0 +1,64 @@
+#----speaker.py by me@qzane.com------#
+#----2016-08-31----------------------#
+
+import sys
+import os
+
+import requests
+from lxml import etree
+
+PLAYER = 'mplayer'
+
+HOST = 'http://dict.cn'
+URL = 'http://audio.dict.cn/{}'
+
+CLASS_MALE = 'sound'
+CLASS_FEMALE = 'sound fsound'
+
+XPATH = '//i[@class=\'{}\']/@naudio'
+
+USAGE = '''USAGE: python3 {PROGRAM_NAME} [options] word
+    -h --help, show this message
+    -F, Female voice (default)
+    -M, Male voice
+    -US, US voice (default)
+    -UK, UK voice
+'''.format(PROGRAM_NAME=sys.argv[0])
+
+def getVoice(word, class_name='F', path='US'):
+    assert(word.isalpha())
+    q = requests.get('%s/%s'%(HOST, word))
+    w = etree.HTML(q.text)
+    
+    e = w.xpath(XPATH.format(CLASS_FEMALE if class_name == 'F' else CLASS_MALE))
+    
+    return None if len(e)==0 else URL.format(e[1] if path == 'US' else e[0])
+
+def getData(word, class_name='F', path='US'):
+    url = getVoice(word, class_name, path)
+    if url is None:
+        return b''
+    else:
+        data = requests.get(url).content
+        return data
+
+def main():
+    data = getData(sys.argv[1])
+    if data == b'':
+        print('no voice found')
+    else:
+        class_name = 'F' if not '-M' in sys.argv else 'M'
+        path = 'US' if not '-UK' in sys.argv else 'UK'
+        with open('tmp.mp3','wb') as f:    
+            f.write(getData(sys.argv[1], class_name, path))
+        os.system('{PLAYER} tmp.mp3'.format(PLAYER=PLAYER))
+
+if __name__ == '__main__':
+    if (len(sys.argv)<2 or
+       '-h' in sys.argv or
+       '--help' in sys.argv):
+        print(USAGE)
+    else:
+        main()
+
+
